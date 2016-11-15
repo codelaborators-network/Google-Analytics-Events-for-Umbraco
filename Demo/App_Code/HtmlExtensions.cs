0f8c13842232811ad0fc71f309cd4d0b6afbecff
Luke Warren <lukejkw@gmail.com>
2016-11-15 18:52:24 +0200

    Added all visual studio stuff

---
+++ Demo/App_Code/HtmlExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace UmbracoGAEventTracking
+{
+    public static class HtmlExtensions
+    {
+        /// <summary>
+        /// Includes google analytics.
+        /// </summary>
+        /// <param name="helper"></param>
+        /// <param name="analyticsKey"></param>
+        /// <returns></returns>
+        public static HtmlString IncludeGoogleAnalytics(this HtmlHelper helper, string analyticsKey)
+        {
+            return helper.Partial("_GoogleAnalytics");
+        }
+
+        /// <summary>
+        /// An HTML Helper that renders Google GA Tracking information
+        /// </summary>
+        /// <param name="helper"></param>
+        /// <returns>MvcHtmlString</returns>
+        public static HtmlString IncludeGaEventTracking(this HtmlHelper helper)
+        {
+            return helper.Partial("_GAEventTracking");
+        }
+    }
+}
